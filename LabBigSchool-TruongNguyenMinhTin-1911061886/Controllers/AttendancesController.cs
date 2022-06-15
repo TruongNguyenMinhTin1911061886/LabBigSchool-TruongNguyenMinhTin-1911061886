85930d82e68b64208640341d2f3e031d3d42366c
Asus <you@example.com>
2022-06-15 17:30:08 +0700

    BigSchool nop bai 15/06/2022 Trang 60

---
+++ LabBigSchool-TruongNguyenMinhTin-1911061886/Controllers/AttendancesController.cs
@@ -0,0 +1,36 @@
+﻿using LabBigSchool_TruongNguyenMinhTin_1911061886.DTOs;
+using LabBigSchool_TruongNguyenMinhTin_1911061886.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LabBigSchool_TruongNguyenMinhTin_1911061886.Controllers
+{
+    public class AttendancesController : ApiController
+    {
+        private ApplicationDbContext _dbContext;
+        public AttendancesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        [HttpPost]
+        public IHttpActionResult Attend(AttendanceDto attendanceDto)
+        {
+            var userId = User.Identity.GetUserId();
+            if (_dbContext.Attendances.Any(a => a.AttendeeId == userId && a.CourseId == attendanceDto.CourseId))
+                return BadRequest("The Attendance already exists!");
+            var attendance = new Attendance
+            {
+                CourseId = attendanceDto.CourseId,
+                AttendeeId = User.Identity.GetUserId()
+            };
+            _dbContext.Attendances.Add(attendance);
+            _dbContext.SaveChanges();
+            return Ok();
+        }
+    }
+}
---
+++ LabBigSchool-TruongNguyenMinhTin-1911061886/DTOs/AttendanceDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LabBigSchool_TruongNguyenMinhTin_1911061886.DTOs
+{
+    public class AttendanceDto
+    {
+        public int CourseId { get; set; }
+    }
+}
